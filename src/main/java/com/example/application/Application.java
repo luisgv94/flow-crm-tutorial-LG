24cf95736d4994d4cf067b492536780b6c776544
luisgv94 <lgomero94@gmail.com>
2023-04-07 13:04:28 -0500

    8. Installation and offline PWA

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,9 +1,11 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
@@ -14,7 +16,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
-public class Application implements AppShellConfigurator {
+@PWA(
+    name = "Vaadin CRM",
+    shortName = "CRM",
+    offlinePath="offline.html",
+    offlineResources = { "./images/offline.png"}
+)
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
