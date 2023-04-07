77490319b01521eedd9ad69438014144dabee39b
luisgv94 <lgomero94@gmail.com>
2023-04-07 17:25:50 -0500

    Final Deploy and omit dashboard

--- src/main/java/com/example/application/views/dashboard/DashboardView.java
+++ src/main/java/com/example/application/views/dashboard/DashboardView.java
@@ -3,10 +3,6 @@ package com.example.application.views.dashboard;
 import com.example.application.data.service.CrmService;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.charts.Chart;
-import com.vaadin.flow.component.charts.model.ChartType;
-import com.vaadin.flow.component.charts.model.DataSeries;
-import com.vaadin.flow.component.charts.model.DataSeriesItem;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -24,7 +20,8 @@ public class DashboardView extends VerticalLayout {
         this.service = service;
         addClassName("dashboard-view");
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        add(getContactStats(), getCompaniesChart());
+//        add(getContactStats(), getCompaniesChart());
+        add(getContactStats());
     }
 
     private Component getContactStats() {
@@ -35,13 +32,13 @@ public class DashboardView extends VerticalLayout {
         return stats;
     }
 
-    private Chart getCompaniesChart() {
-        Chart chart = new Chart(ChartType.PIE);
-
-        DataSeries dataSeries = new DataSeries();
-        service.findAllCompanies().forEach(company ->
-                dataSeries.add(new DataSeriesItem(company.getName(), company.getEmployeeCount())));
-        chart.getConfiguration().setSeries(dataSeries);
-        return chart;
-    }
+//    private Chart getCompaniesChart() {
+//        Chart chart = new Chart(ChartType.PIE);
+//
+//        DataSeries dataSeries = new DataSeries();
+//        service.findAllCompanies().forEach(company ->
+//                dataSeries.add(new DataSeriesItem(company.getName(), company.getEmployeeCount())));
+//        chart.getConfiguration().setSeries(dataSeries);
+//        return chart;
+//    }
 }
